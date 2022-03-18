d143cbb19487d29978f7754a92691279adc8b152
Leon Brands (Desktop) <lajbrands@outlook.com>
2022-03-18 23:45:55 +0100

    Added basic ApplicationModule usage in sandbox

--- applications/sandbox/main.cpp
+++ applications/sandbox/main.cpp
@@ -1,4 +1,18 @@
-int main()
+#define LEGION_ENTRY
+#define LEGION_LOG_DEBUG
+
+#if defined(NDEBUG)
+    #define LEGION_KEEP_CONSOLE
+#endif
+
+#include <core/core.hpp>
+#include <application/application.hpp>
+
+void LEGION_CCONV reportModules(legion::Engine* engine)
 {
-    
+    using namespace legion;
+
+    log::info("Legion core sandbox");
+
+    engine->reportModule<app::ApplicationModule>();
 }
